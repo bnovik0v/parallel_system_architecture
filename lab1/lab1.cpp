cef5c30eb23d49b0da97d49553a7a978e69f18ef
bnovikov <nvikv-brja@yandex.ru>
2020-10-04 17:59:51 +0300

    change 2d array to 1d array

--- lab1/lab1.cpp
+++ lab1/lab1.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <cstdlib>
-#include <mpi/mpi.h>
 
 #include "matrix_multiplication.h"
 
@@ -11,40 +10,49 @@ void setup(int &argc, char **argv, int &size, int &rank);
 
 void endup();
 
-//
-double ** generate2DMatrix(const int &n);
-void randFill2DMatrix(double **M, const int &n);
-void free2DMatrix(double **M, const int &n);
-void show2DMatrix(double **M, const int &n);
+// Matrix
+void generate2DMatrix(double *&M, const int &n);
+void randFill2DMatrix(double *&M, const int &n);
+void free2DMatrix(double *&M);
+void show2DMatrix(double *&M, const int &n);
 
 int main(int argc, char **argv) {
     //MPI_INIT
     int size, rank;
     setup(argc, argv, size, rank);
 
-
     const int N = 4;
-    double **A = nullptr;
-    double **B = nullptr;
-    double **C = nullptr;
+    double *A = nullptr;
+    double *B = nullptr;
+    double *C = nullptr;
 
-    A = generate2DMatrix(N);
-    B = generate2DMatrix(N);
-    C = generate2DMatrix(N);
+    generate2DMatrix(A, N);
+    generate2DMatrix(B, N);
+    generate2DMatrix(C, N);
 
     randFill2DMatrix(A, N);
     randFill2DMatrix(B, N);
 
-    linearMatrixMultiplication(A, B, C, N);
+    double *bufA = new double[4];
+    MPI_Scatter(A, 4, MPI_DOUBLE, bufA, 4, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+    cout << "PR:" << rank << endl;
+    for (int i = 0; i < 4; i++)
+        cout << bufA[i] << " ";
+    cout << endl << endl;
+
+
+    //linearMatrixMultiplication(A, B, C, N);
     parallelMatrixMultiplication(A, B, C, N);
 
-    show2DMatrix(A, N);
+
+    /*show2DMatrix(A, N);
     show2DMatrix(B, N);
     show2DMatrix(C, N);
-
-    free2DMatrix(A, N);
-    free2DMatrix(B, N);
-    free2DMatrix(C, N);
+*/
+    free2DMatrix(A);
+    free2DMatrix(B);
+    free2DMatrix(C);
 
     //MPI_FINALIZE
     endup();
@@ -83,39 +91,32 @@ void endup() {
     }
 }
 
-double ** generate2DMatrix(const int &n)
+void generate2DMatrix(double *&M, const int &n)
 {
-    auto **M = new double*[n];
-    for (int i = 0; i < n; ++i) {
-        M[i] = new double[n];
-        for (int j = 0; j < n; ++j)
-            M[i][j] = 0;
+    M = new double[n*n];
+    for (int i = 0; i < n * n; ++i) {
+        M[i] = 0;
     }
-
-    return M;
 }
 
-void randFill2DMatrix(double **M, const int &n)
+void randFill2DMatrix(double *&M, const int &n)
 {
-    for (int i = 0; i < n; ++i)
-        for (int j = 0; j < n; ++j)
-            M[i][j] = rand();
+    for (int i = 0; i < n * n; ++i)
+            M[i] = rand() % 10;
 }
 
-void free2DMatrix(double **M, const int &n)
+void free2DMatrix(double *&M)
 {
-    for (int i = 0; i < n; ++i)
-       delete []  M[i];
-
     delete [] M;
+    M = nullptr;
 }
 
-void show2DMatrix(double **M, const int &n)
+void show2DMatrix(double *&M, const int &n)
 {
     for (int i = 0; i < n; ++i)
     {
         for (int j = 0; j < n; ++j)
-            cout << M[i][j] << " ";
+            cout << M[i * n + j] << " ";
         cout << endl;
     }
     cout << endl;
--- lab1/matrix_multiplication.h
+++ lab1/matrix_multiplication.h
@@ -1,14 +1,22 @@
 #pragma once
 
-void linearMatrixMultiplication(double **A,  double **B, double **C, const int &n)
+#include <mpi/mpi.h>
+
+void linearMatrixMultiplication(double *&A,  double *&B, double *&C, const int &n)
 {
     for (int i = 0; i < n; ++i)
         for (int j = 0; j < n; ++j)
             for (int k = 0; k < n; ++k)
-                C[i][j] += A[i][k] * B[k][j];
+                C[i * n + j] += A[i * n + k] * B[k * n + j];
 }
 
 void parallelMatrixMultiplication(double **A,  double **B, double **C, const int &n)
 {
+    double *bufA = new double[4];
+    double *bufB = new double[4];
+    MPI_Scatter(A, n, MPI_DOUBLE, bufA, n, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
+
+    delete [] bufA;
+    delete [] bufB;
 }
