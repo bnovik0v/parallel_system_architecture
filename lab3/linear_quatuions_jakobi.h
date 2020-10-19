4dfcaaf171574e56a1f2543420bd5c25ea802376
bnovikov <nvikv-brja@yandex.ru>
2020-10-19 14:36:01 +0300

    add yakobi parallel

--- lab3/linear_quatuions_jakobi.h
+++ lab3/linear_quatuions_jakobi.h
@@ -3,16 +3,17 @@
 #include <cstring>
 #include <cmath>
 
+const double EPS = 0.001;
 
-double calcRelativeX(double *& a, const int & rown, double *& x, double & b, const int & N)
+double calcRelativeX(double *& a, const int & rowN, double *& x, double & b, const int & N)
 {
     double rel_x = 0;
 
     for (int i = 0; i < N; ++i)
-        if (i != rown)
+        if (i != rowN)
             rel_x += a[i] * x[i];
 
-    return (b - rel_x) / a[rown];
+    return (b - rel_x) / a[rowN];
 }
 
 double calcAccuracy(double *& x1, double *& x0, const int & N)
@@ -27,31 +28,63 @@ double calcAccuracy(double *& x1, double *& x0, const int & N)
 
 void yakobi(double *& A, double *& x, double *& b, const int & N)
 {
-    const double EPS = 0.001;
+    auto * curX = new double [N];
+    auto * prevX = new double [N];
 
-    auto * x_cur = new double [N];
-    memcpy(x_cur, x, sizeof(double) * N);
-
-    auto * x_prev = new double [N];
-
-    double *a = nullptr;
+    memcpy(curX, x, sizeof(double) * N);
 
     do {
-        memcpy(x_prev, x_cur, sizeof(double) * N);
+        memcpy(prevX, curX, sizeof(double) * N);
 
         for (int i = 0; i < N; ++i) {
-            a = A + i * N;
-            x_cur[i] = calcRelativeX(a, i, x_prev, b[i], N);
+            double *a = A + i * N;
+            curX[i] = calcRelativeX(a, i, prevX, b[i], N);
         }
-    } while(calcAccuracy(x_cur, x_prev, N) >= EPS);
+    } while(calcAccuracy(curX, prevX, N) > EPS);
 
-    memcpy(x, x_cur, sizeof(double) * N);
+    memcpy(x, curX, sizeof(double) * N);
 
-    delete [] x_cur;
-    delete [] x_prev;
+    delete [] curX;
+    delete [] prevX;
 }
 
-void yakobi_parallel(double *& A, double *& x, double *& b, const int & N)
+void yakobi_parallel(double *& A, double *& x, double *& b, const int & N, const int & size, const int & rank)
 {
+    int taskAmountForProc = N / size;
 
+    int partOfA = N * N / size;
+    auto * bufA = new double [partOfA];
+    MPI_Scatter(A, partOfA, MPI_DOUBLE, bufA, partOfA, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+    int partOfB = N / size;
+    auto * bufB = new double [partOfB];
+    MPI_Scatter(b, partOfB, MPI_DOUBLE, bufB, partOfB, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+    int partOfX = N / size;
+    auto * bufX = new double [partOfB];
+
+    auto * curX = new double [N];
+    memcpy(curX, x, sizeof(double) * N);
+
+    auto *prevX = new double[N];
+
+    do {
+        memcpy(prevX, curX, sizeof(double) * N);
+
+        for (int i = 0; i < taskAmountForProc; ++i) {
+            double *a = A + i * N;
+            int rowN = i + rank * taskAmountForProc;
+            bufX[i] = calcRelativeX(a, rowN, prevX, b[i], N);
+        }
+
+        MPI_Allgather(bufX, partOfX, MPI_DOUBLE, curX, partOfX, MPI_DOUBLE, MPI_COMM_WORLD);
+    } while(calcAccuracy(curX, prevX, N) > EPS);
+
+    memcpy(x, curX, sizeof(double) * N);
+
+    delete [] bufA;
+    delete [] bufB;
+    delete [] bufX;
+    delete [] curX;
+    delete [] prevX;
 }
